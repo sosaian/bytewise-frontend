e61163d1c7d71ce1bfae72cc02a10317465d51ba
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-09-26 18:28:42 -0300

    Add routing for root and default paths in app.jsx
    Add basic styling alongside CSS reset to app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const WIP_MESSAGE = "Página aún en construcción..."
+  const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <header>
+          <h1>BYTEWISE - Tu gestión</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<h2>{WIP_MESSAGE}</h2>} />
+            <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
+          </Routes>
+        </main>
+        <footer>
+          CILSA | Bootcamp Fullstack | Proyecto final
+        </footer>
+      </BrowserRouter>
     </>
   )
 }
