833bba8f62cd54d645ede4e245cb28aeb95845ae
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-07 03:03:01 -0300

    Initial App.jsx rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,48 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RegisterScreen from './RegisterScreen';
-import HomeScreen from './HomeScreen';
-import Dashboard from './Dashboard';
-import Transacciones from './Transacciones';
-import TareasHabitos from './TareasHabitos';
-import logo from './img/color-bw-03.svg';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import RegisterScreen from './RegisterScreen'
+// import HomeScreen from './HomeScreen'
+import Dashboard from './Dashboard'
+import Transacciones from './Transacciones'
+import TareasHabitos from './TareasHabitos'
+import logo from './img/color-bw-03.svg'
+// import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts'
+import './App.css'
 
 function App() {
-  const WIP_MESSAGE = "Página aún en construcción...";
-  const ERROR_MESSAGE = "¡UPS! Esa página no existe...";
+    const WIP_MESSAGE = "Página aún en construcción..."
+    const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
 
-  return (
-    <>
-      <BrowserRouter>
-        <header>
-          <img src={logo} alt="Logo" className="logo" />
-        </header>
-        <main>
-          <Routes>
-            {/* Página principal cuando el usuario está logueado */}
-            <Route path="/" element={<HomeScreen user={{ name: "Pao" }} />} />
+    return (
+        <>
+            <BrowserRouter>
+                <header>
+                    <img src={logo} alt="Logo" className="logo" />
+                </header>
+                <main>
+                    <Routes>
+                    {/* Página principal cuando el usuario está logueado */}
+                    {/* <Route path="/" element={<HomeScreen user={{ name: "Pao" }} />} /> */}
+                    <Route path="/" element={<Navigate to="/login" />}/>
 
-            {/* Página de registro */}
-            <Route path="/register" element={<RegisterScreen />} />
+                    {/* Página de registro y autenticación */}
+                    <Route path="/register" element={<RegisterScreen />} />
+                    <Route path="/login" element={<h2>{WIP_MESSAGE}</h2>} />
 
-            {/* Secciones disponibles desde HomeScreen */}
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/transacciones" element={<Transacciones />} />
-            <Route path="/tareas-habitos" element={<TareasHabitos />} />
+                    {/* Secciones disponibles desde HomeScreen */}
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/transacciones" element={<Transacciones />} />
+                    <Route path="/tareas-habitos" element={<TareasHabitos />} />
 
-            {/* Página en construcción */}
-            <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
-          </Routes>
-        </main>
-        <footer>
-          CILSA | Bootcamp Fullstack | Proyecto final
-        </footer>
-      </BrowserRouter>
-    </>
-  );
+                    {/* Página que no existe */}
+                    <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
+                    </Routes>
+                </main>
+                <footer>
+                    CILSA | Bootcamp Fullstack | Proyecto final
+                </footer>
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App;
+export default App
