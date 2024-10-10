fed437c5285a43bf843a6bd22819c8956f5f8a5a
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-10 02:22:42 -0300

    Add LogoutButton functionality to clear client's auth cookies

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,13 +1,42 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
 import './LogoutButton.css'
-import { Link } from 'react-router-dom'
 
 export function LogoutButton()
 {
+    const navigateTo = useNavigate()
+
+    const [logoutInProcess, setLogoutInProcess] = useState(false)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        setLogoutInProcess(true)
+
+        fetch(import.meta.env.VITE_AUTH_LOGOUT_URL, {
+            method: 'POST',
+            credentials: 'include'
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .then(() => {
+            setLogoutInProcess(false)
+            
+            Swal.fire({
+            title: "CIERRE DE SESIÓN EXISTOSO!",
+            html: `¡Nos vemos la próxima!`,
+            icon: "success"
+            }).then(() => navigateTo('/login'))
+        })
+        .catch(error => console.error(error))
+    }
+
     return (
         <>
-            <Link to={'/'}>
-                <button>👋🏿 Cerrar Sesión</button>
-            </Link>
+            <button onClick={handleSubmit}>
+                {logoutInProcess ? "AGUARDA UNOS INSTANTES": "👋🏿 Cerrar Sesión"}
+            </button>
         </>
     )
 }
