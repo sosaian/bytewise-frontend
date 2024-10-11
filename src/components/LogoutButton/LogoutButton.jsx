463905871241d9149abf08ee9a3cbf114d724c6d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-11 05:51:48 -0300

    Add login context implementation to LogoutButton
    Rename to handleLogout
    
    Refactor to also improve UX

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,40 +1,54 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { LoginContext } from '../../context/LoginContext'
 import Swal from 'sweetalert2'
 import './LogoutButton.css'
 
-export function LogoutButton()
-{
+export function LogoutButton(){
+    const { clearLogin } = useContext(LoginContext)
+
     const navigateTo = useNavigate()
 
     const [logoutInProcess, setLogoutInProcess] = useState(false)
 
-    const handleSubmit = (event) => {
+    const handleLogout = (event) => {
         event.preventDefault()
 
-        setLogoutInProcess(true)
+        Swal.fire({
+            title: "Estás por cerrar sesión... ¿Continuar?",
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: "Cerrar sesión",
+            denyButtonText: "Cancelar"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                setLogoutInProcess(true)
 
-        fetch(import.meta.env.VITE_AUTH_LOGOUT_URL, {
-            method: 'POST',
-            credentials: 'include'
+                fetch(import.meta.env.VITE_AUTH_LOGOUT_URL, {
+                    method: 'POST',
+                    credentials: 'include'
+                })
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .then(() => {
+                    setLogoutInProcess(false)
+                    
+                    Swal.fire({
+                    title: "CIERRE DE SESIÓN EXISTOSO!",
+                    html: `¡Nos vemos la próxima!`,
+                    icon: "success"
+                    }).then(() => {
+                        clearLogin()
+                        navigateTo('/login')
+                    })
+                })
+                .catch(error => console.error(error))
+            }
         })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .then(() => {
-            setLogoutInProcess(false)
-            
-            Swal.fire({
-            title: "CIERRE DE SESIÓN EXISTOSO!",
-            html: `¡Nos vemos la próxima!`,
-            icon: "success"
-            }).then(() => navigateTo('/login'))
-        })
-        .catch(error => console.error(error))
     }
-
     return (
         <>
-            <button onClick={handleSubmit}>
+            <button onClick={handleLogout}>
                 {logoutInProcess ? "AGUARDA UNOS INSTANTES": "👋🏿 Cerrar Sesión"}
             </button>
         </>
