062699cb7444e68117ba3f60cb59d56bcd675350
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-12 18:17:48 -0300

    Add basic implementation for task creation
    This implementation makes use of 'html' display in sweetalert2 modals to save valus in inputs that are later used in handleSubmit()
    
    handleSubmit() then calls createTask() after input values are verified
    
    TODO: check how this component could be modularized properly

--- src/components/TaskListContainer/TaskListContainer.jsx
+++ src/components/TaskListContainer/TaskListContainer.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
 import './TaskListContainer.css'
 
 export function TaskListContainer() {
@@ -11,9 +12,7 @@ export function TaskListContainer() {
         })
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
             setTasks(data)
-            console.log(tasks)
         })
         .catch(err => console.error(err))
     },[])
@@ -58,33 +57,135 @@ export function TaskListContainer() {
         return `${day}-${month}-${year}`;
     }
 
+    const createTask = (NEW_TASK) => {
+        fetch(import.meta.env.VITE_TASK_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(NEW_TASK),
+            credentials: 'include'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log((data.id) ? "Task creation successful" : "Task creation failed")
+        })
+        .then(() => {            
+            Swal.fire({
+                title: "Todo ta bien",
+                html: `Task creada exitosamente.`,
+                icon: "success"
+            })
+        })
+        .catch(error => console.error(error))
+    }
+    
+    const handleSubmit = () => {
+        const DESCRIPTION = document.getElementById("taskFormDescription").value.trim()
+        const STATUS = document.getElementById("taskFormStatus").value
+        const DUE_DATE = document.getElementById("taskFormDueDate").value
+
+        if (DESCRIPTION === "") {
+            Swal.fire({
+                title: "Error al crear tarea",
+                text: "El nombre o descripción de la tarea no es válida, por favor vuelva a intentarlo.",
+                icon: "error"
+            })
+
+            return
+        }
+
+        if (STATUS === "") {
+            Swal.fire({
+                title: "Error al crear tarea",
+                text: "Es necesario elegir algún estado actual para la tarea, por favor vuelva a intentarlo.",
+                icon: "error"
+            })
+
+            return
+        }
+
+        if (DUE_DATE === "") {
+            Swal.fire({
+                title: "Error al crear tarea",
+                text: "Es necesario tener alguna fecha límite para la tarea, por favor vuelva a intentarlo.",
+                icon: "error"
+            })
+
+            return
+        }
+
+        const NEW_TASK = {
+            description_task: DESCRIPTION,
+            status_task: STATUS,
+            due_date: DUE_DATE
+        }
+
+        createTask(NEW_TASK)
+    }
+
+    const createTaskForm = () => {
+        Swal.fire({
+            title: 'Crear nueva tarea',
+            html: `
+                <div>
+                    <label for="taskFormDescription" class="swal2-input">Nombre o descripción de la tarea</label>
+                    <input id="taskFormDescription" class="swal2-input" type="text" placeholder="Lo que quiero hacer" required />
+                </div>
+                <div>
+                    <label for="taskFormStatus">Estado de la tarea</label>
+                    <select id="taskFormStatus" class="swal2-input" required>
+                        <option value="" disabled selected>Seleccionar estado</option>
+                        <option value="not_started">Pendiente</option>
+                        <option value="in_progress">En progreso</option>
+                        <option value="complete">Terminada</option>
+                        <option value="overdue">Atrasada</option>
+                        <option value="canceled">Cancelada</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="taskFormDueDate">Fecha límite</label>
+                    <input id="taskFormDueDate" type="date" class="swal2-input" required />
+                </div>
+            `,
+            showCancelButton: true,
+            confirmButtonText: "Crear Tarea",
+            cancelButtonText: "Cancelar"
+        }).then((result) => {
+            if (result.isConfirmed) handleSubmit()
+        })
+    }
+
     return (
         <div className="task-list-container">
-        <h2>Tareas del usuario</h2>
+            <div>
+                <h2>Tareas del usuario</h2>
+                <button onClick={createTaskForm}><img src="./assets/icon/icon_add.svg" alt="Add icon"/> Nueva tarea</button>
+            </div>
 
-        <table className="task-table">
-            <thead>
-            <tr>
-                <th>Descripción</th>
-                <th>Estado de la tarea</th>
-                <th>Fecha límite</th>
-                <th></th>
-                <th></th>
-            </tr>
-            </thead>
-
-            <tbody>
-            {tasks.map((task) => (
-                <tr key={task.id}>
-                    <td>{task.description_task}</td>
-                    <td className={`estado ${task.status_task.replace(' ', '-')}`}>{translateStatus(task.status_task)}</td>
-                    <td>{formatDate(task.due_date)}</td>
-                    <td><img src="./assets/icon/icon_edit.svg" alt="Edit icon"/></td>
-                    <td><img src="./assets/icon/icon_delete.svg" alt="Delete icon"/></td>
+            <table className="task-table">
+                <thead>
+                <tr>
+                    <th>Descripción</th>
+                    <th>Estado de la tarea</th>
+                    <th>Fecha límite</th>
+                    <th></th>
+                    <th></th>
                 </tr>
-            ))}
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                {tasks.map((task) => (
+                    <tr key={task.id}>
+                        <td>{task.description_task}</td>
+                        <td className={`estado ${task.status_task.replace(' ', '-')}`}>{translateStatus(task.status_task)}</td>
+                        <td>{formatDate(task.due_date)}</td>
+                        <td><img src="./assets/icon/icon_edit.svg" alt="Edit icon"/></td>
+                        <td><img src="./assets/icon/icon_delete.svg" alt="Delete icon"/></td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     )
 }
