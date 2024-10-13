8875cb387d57a912e18bf3521381a56d5807c01a
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-10-13 18:27:17 -0300

    Add checkLogin() to LoginContext for protected routes proper verification

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -5,12 +5,33 @@ export const LoginContext = createContext()
 export const LoginComponentContext = ({ children }) => {
     const [login, setLogin] = useState({})
 
+    const checkLogin = async () => {
+        try {
+            const response = await fetch(import.meta.env.VITE_TASK_GET_ALL_URL, {
+                method: 'GET',
+                credentials: 'include'
+            })
+
+            const responseData = await response.json()
+            
+            if (responseData.name) {
+                setLogin({ valid: true, name: responseData.name })
+                return true
+            } else {
+                return false
+            }
+        } catch (error) {
+            console.error(error)
+            return false
+        }
+    }
+
     const clearLogin = () => {
         setLogin({})
     }
 
     return (
-        <LoginContext.Provider value={ { login, setLogin, clearLogin } }>
+        <LoginContext.Provider value={ { login, setLogin, checkLogin, clearLogin } }>
             {children}
         </LoginContext.Provider>
     )
